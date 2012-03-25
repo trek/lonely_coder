d24f6e6420d3e4f7a9e605ea5de03807db157e6f
Trek Glowacki <trek.glowacki@gmail.com>
2012-03-25 01:04:43 -0400

    version bump

--- lib/lonely_coder.rb
+++ lib/lonely_coder.rb
@@ -11,7 +11,7 @@ require 'mechanize'
 
 class OKCupid
   BaseUrl = 'http://www.okcupid.com'
-  VERSION = '0.1.1'
+  VERSION = '0.1.2'
   
   def initialize(username=nil, password=nil)
     @browser = Mechanize.new 
