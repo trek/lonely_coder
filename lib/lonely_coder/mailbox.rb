b499b84454de774588d3cc1299c4feaae01fe284
Trek Glowacki <trek.glowacki@gmail.com>
2012-03-26 13:57:57 -0400

    Added mailbox/conversation

---
+++ lib/lonely_coder/mailbox.rb
@@ -0,0 +1,124 @@
+require 'date'
+
+class OKCupid
+  def mailbox
+    @mailbox ||= Mailbox.new(@browser)
+  end
+  
+  def conversation_for(id)
+    Mailbox::Conversation.by_id(id, @browser)
+  end
+  
+  class Mailbox
+    class MessageSnippet
+      
+      attr_accessor :profile_username, :profile_small_avatar_url, :preview, :last_date, :conversation_url
+      
+      def self.from_html(html)
+        profile_username = html.search('a.subject').text
+        preview = html.search('.previewline').text
+        last_date = html.search('.timestamp').text
+        conversation_url = html.search('p:first').attribute('onclick').text.gsub('window.location=\'', '').gsub('\';','')
+        profile_small_avatar_url = html.search('a.photo img').attribute('src').text
+        
+        self.new({
+          profile_username: profile_username,
+          preview: preview,
+          last_date: Date.parse(last_date),
+          conversation_url: conversation_url,
+          profile_small_avatar_url: profile_small_avatar_url
+        })
+      end
+      
+      def initialize(attrs)
+        attrs.each do |attr, value|
+          self.send("#{attr}=", value)
+        end
+      end
+    end
+    
+    class Conversation
+      attr_accessor :from_profile_username, :messages
+      
+      def self.by_id(id, browser)
+        html = browser.get("/messages?readmsg=true&threadid=#{id}&folder=1")
+        from_profile_username = html.search('li.to_me:first a').attribute('title').text
+         
+        messages = []
+        
+        html.search('#thread  > li').each do |message_html|
+          css_class = message_html.attribute('class')
+          css_id    = message_html.attribute('id')
+          
+          if (css_class && css_class.text.match(/_me/))
+            if(css_id && css_id.text == 'compose')
+              next
+            else
+              messages << Message.from_html(message_html)
+            end
+          else
+            next
+          end
+        end
+        
+        self.new({
+          from_profile_username: from_profile_username,
+          messages: messages
+        })
+      end
+      
+      def initialize(attrs)
+        attrs.each do |attr, value|
+          self.send("#{attr}=", value)
+        end
+      end
+    end
+    
+    class Message
+      attr_accessor :to_me, :from_me, :body
+      
+      def self.from_html(html)
+        to_me = !!html.attribute('class').text.match(/to_me/)
+        from_me = !to_me
+        # time = html.search('.timestamp').text
+        body = html.search('.message_body').text.gsub('<br>', "\n")
+        
+        self.new({
+          to_me: to_me,
+          from_me: from_me,
+          # time: time,
+          body: body
+        })
+      end
+      
+      def initialize(attrs)
+        attrs.each do |attr, value|
+          self.send("#{attr}=", value)
+        end
+      end
+    end
+    
+    def initialize(browser)
+      @browser = browser
+    end
+    
+    def useage
+      html = @browser.get('/messages')
+      current, max = html.search('p.fullness').text.match(/([\d]+) of ([\d]+)/).captures
+      
+      return { current: current.to_i, max: max.to_i }
+    end
+    
+    def messages
+      @messages = []
+      
+      html = @browser.get('/messages')
+      messages_html = html.search('#messages li')
+      @messages += messages_html.collect do |message|
+        MessageSnippet.from_html(message)
+      end
+      
+      @messages
+    end
+  end
+end
---
+++ spec/mailbox_spec.rb
@@ -0,0 +1,91 @@
+require 'spec_helper'
+
+describe "Mailbox" do
+  it "tells you how full your mailbox is" do
+    VCR.use_cassette('loading_mailbox', :erb => {username: ENV['OKC_USERNAME'], password: ENV['OKC_PASSWORD']}) do
+      okc = OKCupid.new(ENV['OKC_USERNAME'], ENV['OKC_PASSWORD'])
+      @mailbox = okc.mailbox
+      @mailbox.useage.should == {
+        current: 233,
+        max: 300
+      }
+    end
+  end
+  
+  it "can access the first message, up to 30" do
+    VCR.use_cassette('loading_mailbox', :erb => {username: ENV['OKC_USERNAME'], password: ENV['OKC_PASSWORD']}) do
+      okc = OKCupid.new(ENV['OKC_USERNAME'], ENV['OKC_PASSWORD'])
+      @mailbox = okc.mailbox
+      @mailbox.messages.count.should == 30
+    end
+  end
+  
+  it "each message header is a header" do
+    VCR.use_cassette('loading_mailbox', :erb => {username: ENV['OKC_USERNAME'], password: ENV['OKC_PASSWORD']}) do
+      okc = OKCupid.new(ENV['OKC_USERNAME'], ENV['OKC_PASSWORD'])
+      @mailbox = okc.mailbox
+      @mailbox.messages.all? {|m| m.is_a?(OKCupid::Mailbox::MessageSnippet)}.should == true
+    end
+  end
+end
+
+describe "Conversation" do
+  before(:each) do
+    VCR.use_cassette('loading_conversation') do
+      okc = OKCupid.new(ENV['OKC_USERNAME'], ENV['OKC_PASSWORD'])
+      @conversation = okc.conversation_for('5887692615523576083')
+    end
+  end
+  
+  it "has a from_profile_username" do
+    @conversation.from_profile_username.should == 'snowpea383'
+  end
+  
+  it "contains a list of messages" do
+    @conversation.messages.count.should == 8
+  end
+  
+  describe "each message" do
+    before(:each) do
+      @message = @conversation.messages.last
+    end
+    
+    it "has a to_me" do
+      @message.to_me.should == true
+    end
+    
+    it "has a from_me" do
+      @message.from_me.should == false
+    end
+  end
+end
+
+describe "MessageSnippet" do
+  before(:each) do
+    VCR.use_cassette('loading_mailbox', :erb => {username: ENV['OKC_USERNAME'], password: ENV['OKC_PASSWORD']}) do
+      okc = OKCupid.new(ENV['OKC_USERNAME'], ENV['OKC_PASSWORD'])
+      mailbox = okc.mailbox
+      @header = mailbox.messages.first
+    end
+  end
+  
+  it "has a profile_username" do
+    @header.profile_username.should == 'teachforall'
+  end
+  
+  it "has a profile_small_avatar_url" do
+    @header.profile_small_avatar_url.should == 'http://ak2.okccdn.com/php/load_okc_image.php/images/60x60/60x60/0x30/198x228/2/18256810077890846020.jpeg'
+  end
+  
+  it "has a preview" do
+    @header.preview.should == 'No, I was there like a month ago. I live in EL so  ...'
+  end
+  
+  it "has a conversation_url" do
+    @header.conversation_url.should == '/messages?readmsg=true&threadid=9950201897626358080&folder=1'
+  end
+  
+  it "has a last_date" do
+    @header.last_date.should == Date.new(2012, 03, 25)
+  end
+end
