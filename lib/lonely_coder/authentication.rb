fe3a8fef9e16e95d0b5cb7d4dd21a7d64a7f73f6
Trek Glowacki <trek@groupon.com>
2014-02-03 19:27:06 -0600

    Fixes Authentication
    OKCupid added a form param and now responds with JSON for authentication

--- lib/lonely_coder/authentication.rb
+++ lib/lonely_coder/authentication.rb
@@ -1,37 +1,45 @@
+require 'json'
+
 class OKCupid
-  
+
   def authenticate(username, password)
     @authentication = Authentication.new(username, password, @browser)
   end
-  
+
   class Authentication
     def initialize(username, password, browser)
       change_to_using_simpler_parser(browser)
-      
+
       browser.post("https://www.okcupid.com/login", {
         username: username,
-        password: password
+        password: password,
+        okc_api: 1
       })
 
-      @success = browser.page.uri.path == '/home'
-      
+      body = JSON.parse(browser.page.body)
+
+      # body['screenname'] will be `nil` for failed auth
+      # and equal to the supplied username for success.
+      # there may be other cases?
+      @success = body['screenname'] != nil
+
       restore_default_parser(browser)
     end
-    
+
     def success?
       @success
     end
-    
+
     def change_to_using_simpler_parser(browser)
       browser.pluggable_parser.html = AuthenticationParser
     end
-    
+
     def restore_default_parser(browser)
       browser.pluggable_parser.html = Mechanize::Page
     end
   end
-  
-  class AuthenticationParser < Mechanize::Page  
+
+  class AuthenticationParser < Mechanize::Page
     # We're only using page uri to determine successful login, so
     # there's not a lot of value in passing a body string to nokogiri
     def initialize(uri = nil, response = nil, body = nil, code =nil)
